0a5bbf6664838b37ce9db1b824b49c4b4d92d73b
MYH <m_lovea@naver.com>
2022-06-23 16:41:51 +0900

    Commit Test

---
+++ src/main/java/com/attoresearchtest/AttoreSearchTestApplication.java
@@ -0,0 +1,13 @@
+package com.attoresearchtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttoreSearchTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AttoreSearchTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/attoresearchtest/AttoreSearchTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attoresearchtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttoreSearchTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
